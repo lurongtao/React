2b67b7c40b34b79399f74f83d3d4c669c4064ca4
lurongtao <lurongtao@126.com>
2018-10-01 13:44:36 +0800

    init

---
+++ basic/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import GetStarted from './styled-components/01-get-started'
+
+class App extends Component {
+  render() {
+    return (
+      <GetStarted></GetStarted>
+    );
+  }
+}
+
+export default App;
